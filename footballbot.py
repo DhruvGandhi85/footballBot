546b608a982e1c3c4dd6711f069e9fc122c66578
Dhruv Gandhi <dhruvgandhi@gmail.com>
2023-03-07 16:26:48 -0500

    reqs

--- footballbot.py
+++ footballbot.py
@@ -259,6 +259,6 @@ async def send_chart_as_attachment(channel, chart_path):
         chart = discord.File(f)
         await channel.send(file=chart)
 
-with open(".env", "r") as token:
-    token = token.read()
+
+token = os.environ['TOKEN']
 client.run(token)
